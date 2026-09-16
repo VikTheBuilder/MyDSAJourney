1e86cbe9acbe0a34293e8c52290e02a8e2b9571d
Vikramaditya Singh <singhvikramaditya25@gmail.com>
2026-09-16 16:07:58 +0530

    Implement deleteNode method for BST

--- Delete Node in BST.java
+++ Delete Node in BST.java
@@ -13,3 +13,37 @@
  *     }
  * }
  */
+
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        if(root.val == key) return helper(root);
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.val > key){
+                if(curr.left != null && curr.left.val == key){
+                    curr.left = helper(curr.left);
+                }
+                else curr = curr.left;
+            } else{
+                if(curr.right != null && curr.right.val == key){
+                    curr.right = helper(curr.right);
+                }
+                else curr = curr.right;
+            }
+        }
+        return root;
+    }
+    public TreeNode helper(TreeNode root){
+        if(root.left == null) return root.right;
+        if(root.right == null) return root.left;
+        TreeNode rightChild = root.right;
+        TreeNode lastRight = findLast(root.left);
+        lastRight.right = root.right;
+        return root.left;
+    }
+    public TreeNode findLast(TreeNode root){
+        while(root.right != null) root = root.right;
+        return root;
+    }
+}
